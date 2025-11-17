79b3fcf800cc60ebc4d38d0a82f4f090965f5195
Somayeh Hosseinbeig <somayeh.hosseinbeig@gmail.com>
2025-11-17 14:33:12 +0330

    static and media root
    created template and static DIR

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from website.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('url address','view',name)
     path('', include('website.urls'))
 ]
+# static('static'.base/static)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
