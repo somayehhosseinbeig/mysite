d8650a30ffe57d2bff69c08eb6e8b164deded0e9
Somayeh Hosseinbeig <somayeh.hosseinbeig@gmail.com>
2025-12-15 14:33:18 +0330

    makemigration and migrate

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -6,4 +7,7 @@ def blog_view(request):
   return render(request,'blog/blog-home.html')
 
 def blog_single(request):
-  return render(request,'blog/blog-single.html')
+  context = {'title':'title subject',
+  'content':'jhsdgfsdj dskjfhsd fdsjfhfjsd hjdsfhfj fdsjfh',
+  'author':'Somayeh Hosseinbeig'}
+  return render(request,'blog/blog-single.html',context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('url address','view',name)
     path('', include('website.urls')),
-    path('blog', include('blog.urls'))
+    path('blog/', include('blog.urls'))
 ]
 # static('static'.base/static)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
